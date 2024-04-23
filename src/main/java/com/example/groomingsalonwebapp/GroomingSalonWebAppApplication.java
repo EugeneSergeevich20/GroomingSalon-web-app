ba58882092cc697b6e173e1a639de4696615f2d5
EugeneSergeevich20 <eugene.chernitsyn15@gmail.com>
2024-04-23 19:10:52 +0400

    First-commit

---
+++ src/main/java/com/example/groomingsalonwebapp/GroomingSalonWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.groomingsalonwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomingSalonWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroomingSalonWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/groomingsalonwebapp/GroomingSalonWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groomingsalonwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomingSalonWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
