3a155a85809b55f2544a1afdcb695ded212a7965
EugeneSergeevich20 <eugene.chernitsyn15@gmail.com>
2024-05-01 19:08:59 +0400

    Реализован личный кабинет

---
+++ src/main/java/com/example/groomingsalonwebapp/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.groomingsalonwebapp.controller;
+
+import com.example.groomingsalonwebapp.model.User;
+import com.example.groomingsalonwebapp.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    private final UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/home")
+    public String homePage(Model model){
+
+        User user = userService.getAuthUser();
+        model.addAttribute("user", user);
+
+        return "index";
+    }
+
+}
