dd9c2b7274c7f16c52ef1df5843324b6bb9e83d0
EugeneSergeevich20 <eugene.chernitsyn15@gmail.com>
2024-05-27 11:44:42 +0400

    Обновил код в новых страницах

--- src/main/java/com/example/groomingsalonwebapp/controller/ServiceController.java
+++ src/main/java/com/example/groomingsalonwebapp/controller/ServiceController.java
@@ -3,9 +3,11 @@ package com.example.groomingsalonwebapp.controller;
 import com.example.groomingsalonwebapp.model.Appointment;
 import com.example.groomingsalonwebapp.model.Category;
 import com.example.groomingsalonwebapp.model.Service;
+import com.example.groomingsalonwebapp.model.User;
 import com.example.groomingsalonwebapp.service.AppointmentService;
 import com.example.groomingsalonwebapp.service.CategoryService;
 import com.example.groomingsalonwebapp.service.ServicesService;
+import com.example.groomingsalonwebapp.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +18,13 @@ import java.util.List;
 @Controller
 public class ServiceController {
 
+    private final UserService userService;
     private final ServicesService service;
     private final CategoryService categoryService;
     public final AppointmentService appointmentService;
 
-    public ServiceController(ServicesService service, CategoryService categoryService, AppointmentService appointmentService) {
+    public ServiceController(UserService userService, ServicesService service, CategoryService categoryService, AppointmentService appointmentService) {
+        this.userService = userService;
         this.service = service;
         this.categoryService = categoryService;
         this.appointmentService = appointmentService;
@@ -28,6 +32,9 @@ public class ServiceController {
 
     @GetMapping("/home")
     public String getServices(Model model){
+        User userAuth = userService.getAuthUser();
+        model.addAttribute("user", userAuth);
+
         List<Category> categories = categoryService.getCategoryList();
 
         HashMap<String, Category> nameCategories = null;
